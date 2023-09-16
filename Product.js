cb318b511c1767640e78b7dcbefb3fba238ad5b2
BHARATH KISHORE <94332847+bharathkishore2023@users.noreply.github.com>
2023-09-16 17:47:04 +0530

    Product component

---
+++ Product.js
@@ -0,0 +1,279 @@
+import React from 'react'
+import "./Product.css"
+const Product = () => {
+  return (
+    <div class="container d-flex justify-content-center mt-50 mb-50">
+            
+    <div class="row">
+       <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                                    C:\Users\Pradheepa.P\Desktop\Rendal Ride\rendal\src\components\Product.js
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                    <img src="https://i5.walmartimages.com/asr/0f9c5a43-21e2-4859-a065-aa723a7fa617_1.6695fc7cfb43cf7e3fba42d38ca0d58f.jpeg" alt="Girl in a jacket" width="500" height="600"/>
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>
+
+
+                        
+                         
+       </div> 
+
+
+       {/* <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                                           
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>
+
+
+                        
+                         
+       </div> 
+
+       <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                                         
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>
+
+
+                        
+                         
+       </div> 
+
+
+       <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                            
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>
+
+
+                        
+                         
+       </div> 
+
+
+       <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                                          
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>
+
+
+                        
+                         
+       </div> 
+
+
+       <div class="col-md-4 mt-2">
+        
+            
+            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-img-actions">
+                                        
+                                         
+                                          
+                                       
+                                    </div>
+                                </div>
+
+                                <div class="card-body bg-light text-center">
+                                    <div class="mb-2">
+                                        <h6 class="font-weight-semibold mb-2">
+                                            <a href="#" class="text-default mb-2" data-abc="true">Toshiba Notebook with 500GB HDD & 8GB RAM</a>
+                                        </h6>
+
+                                        <a href="#" class="text-muted" data-abc="true">Laptops & Notebooks</a>
+                                    </div>
+
+                                    <h3 class="mb-0 font-weight-semibold">$250.99</h3>
+
+                                    <div>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                       <i class="fa fa-star star"></i>
+                                    </div>
+
+                                    <div class="text-muted mb-3">34 reviews</div>
+
+                                    <button type="button" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i> Add to cart</button>
+
+                                    
+                                </div>
+                            </div>                    
+                         
+       </div>  */}
+
+    </div>
+</div>
+  )
+}
+
+export default Product;
